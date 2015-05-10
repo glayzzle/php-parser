804b311b1d3b1b9428d065c22f67fab87c60a2b0
Ioan CHIRIAC <i.chiriac@yahoo.com>
2015-05-10 13:17:05 +0200

    implement try / catch / finally

--- src/parser/try.js
+++ src/parser/try.js
@@ -7,12 +7,37 @@ module.exports = function(api, tokens, EOF) {
   return {
     /**
      * <ebnf>
-     *  try ::= T_TRY
+     *  try ::= T_TRY '{' inner_statement* '}' 
+     *          (
+     *              T_CATCH '(' namespace_name variable ')' '{'  inner_statement* '}' 
+     *          )* 
+     *          (T_FINALLY '{' inner_statement* '}')?
      * </ebnf>
      */
     read_try: function() {
+
+      // @todo implement the short form of declarations
       this.expect(tokens.T_TRY).next();
-      // @todo
+
+      var code = this.read_statement();
+      var allways = false;
+      var catches = [];
+
+      while(this.token === tokens.T_CATCH) {
+        this.next().expect('(').next();
+        var exName = this.read_namespace_name();
+        var varName = this.read_variable(true);
+        this.expect(')').next();
+        catches.push({
+          exception: exName,
+          as: varName,
+          body: this.read_statement()
+        });
+      }
+      if (this.token === tokens.T_FINALLY) {
+        allways = this.next().read_statement();
+      }
+      return ['try', code, catches, allways];
     }
   };
 };
