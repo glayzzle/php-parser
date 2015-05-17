b5792121dceb007bee8b55232387593ee885e664
Ioan CHIRIAC <i.chiriac@yahoo.com>
2015-05-17 18:45:48 +0200

    fix utf8 errors

--- bin/formats/token.php
+++ bin/formats/token.php
@@ -8,7 +8,16 @@
   $tokens = token_get_all(
     file_get_contents($argv[1])
   );
+  $last = count($tokens) - 1;
+  echo '[';
   foreach($tokens as $p => $t) {
-    if (is_array($t)) $tokens[$p][0] = token_name($t[0]);
+    if (is_array($t)) {
+      $t[0] = token_name($t[0]);
+      $t[1] = utf8_encode($t[1]);
+    }
+    echo json_encode( $t );
+    if ($p !== $last) {
+      echo ',';
+    }
   }
-  echo json_encode( $tokens );
+  echo ']';
