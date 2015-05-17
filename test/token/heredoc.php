96f7e86901c7d175d1ea6aad2b4e48b7d84e8bde
Ioan CHIRIAC <i.chiriac@yahoo.com>
2015-05-17 14:42:28 +0200

    more tests

--- test/token/heredoc.php
+++ test/token/heredoc.php
@@ -1,33 +1,24 @@
 <?php
 
-/* @todo : should pass :
+$fallbackContent .= sprintf(<<<EOF2
+\$catalogue%s = new MessageCatalogue('%s', %s);
+\$catalogue%s->addFallbackCatalogue(\$catalogue%s);
+EOF2
+);
+
+/* @todo : should pass : */
   $js =<<<"EOJ"
   Yo man
 EOJ
     . "text";
-    
-    $opml = <<<EOF
-   text="{$feeds[0]['title[0][value]']}"
-EOF;
 
-        'expected' => <<<'EXPECTED'
+    $opml = <<<EOF1
+   text="{$feeds[0]['title[0][value]']}"
+EOF1;
+
+  $expected = <<<'EXPECTED'
 $no_index_value_scalar = TRUE;
 $no_index_value_foo['foo']['value'] = NULL; // comment
 EXPECTED
-      ),
+;
 
-    if ($private) {
-      $lines = <<<EOF
-# Deny all requests from Apache 2.4+.
-<IfModule mod_authz_core.c>
-  Require all denied
-</IfModule>
-
-# Deny all requests from Apache 2.0-2.2.
-<IfModule !mod_authz_core.c>
-  Deny from all
-</IfModule>
-EOF
-      . $lines;
-*/
-  echo 'Hello World';
