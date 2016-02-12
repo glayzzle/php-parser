7293073508ef34e40582a1f1b755a07fd4157f22
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-02-12 14:45:51 +0100

    replace tabs with 2 spaces

--- main.js
+++ main.js
@@ -5,46 +5,46 @@
  */
 var extend = require('util')._extend;
 var engine = {
-	// parsing eval string as '$x = 1;'
-	parseEval: function(buffer, options) {
-		var lexer = this.lexer;
-		var parser = this.parser;
-		if (options) {
-			if (options.hasOwnProperty('lexer')) {
-				lexer = extend(lexer, options.lexer);
-			}
-			if (options.hasOwnProperty('parser')) {
-				parser = extend(parser, options.parser);
-			}
-			parser.lexer = lexer;
-		}
-		lexer.mode_eval = true;
-		lexer.all_tokens = false;
-		return parser.parse(buffer);
-	}
-	// parse php code with '<?php $x = 1;'
-	,parseCode: function(buffer, options) {
-		var lexer = this.lexer;
-		var parser = this.parser;
-		if (options) {
-			if (options.hasOwnProperty('lexer')) {
-				lexer = extend(lexer, options.lexer);
-			}
-			if (options.hasOwnProperty('parser')) {
-				parser = extend(parser, options.parser);
-			}
-			parser.lexer = lexer;
-		}
-		lexer.mode_eval = false;
-		lexer.all_tokens = false;
-		return parser.parse(buffer);
-		}
-	}
-	,parser: null
-	// lexer instance
-	,lexer: require('./src/lexer')
-	// tokens dictionnary
-	,tokens: require('./src/tokens')
+  // parsing eval string as '$x = 1;'
+  parseEval: function(buffer, options) {
+    var lexer = this.lexer;
+    var parser = this.parser;
+    if (options) {
+      if (options.hasOwnProperty('lexer')) {
+        lexer = extend(lexer, options.lexer);
+      }
+      if (options.hasOwnProperty('parser')) {
+        parser = extend(parser, options.parser);
+      }
+      parser.lexer = lexer;
+    }
+    lexer.mode_eval = true;
+    lexer.all_tokens = false;
+    return parser.parse(buffer);
+  }
+  // parse php code with '<?php $x = 1;'
+  ,parseCode: function(buffer, options) {
+    var lexer = this.lexer;
+    var parser = this.parser;
+    if (options) {
+      if (options.hasOwnProperty('lexer')) {
+        lexer = extend(lexer, options.lexer);
+      }
+      if (options.hasOwnProperty('parser')) {
+        parser = extend(parser, options.parser);
+      }
+      parser.lexer = lexer;
+    }
+    lexer.mode_eval = false;
+    lexer.all_tokens = false;
+    return parser.parse(buffer);
+    }
+  }
+  ,parser: null
+  // lexer instance
+  ,lexer: require('./src/lexer')
+  // tokens dictionnary
+  ,tokens: require('./src/tokens')
 };
 // parser instance
 engine.parser = require('./src/parser')(engine);
