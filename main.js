cdbe10abad7b1202bd18b01e04a2b0a7d868afe1
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-02-12 14:42:40 +0100

    makes the parser extensible (can rewrite any lexer or parser function or option)

--- main.js
+++ main.js
@@ -3,24 +3,48 @@
  * @authors https://github.com/glayzzle/php-parser/graphs/contributors
  * @url http://glayzzle.com
  */
+var extend = require('util')._extend;
 var engine = {
-  // parsing eval string as '$x = 1;'
-  parseEval: function(buffer) {
-    this.lexer.mode_eval = true;
-    this.lexer.all_tokens = false;
-    return this.parser.parse(buffer);
-  }
-  // parse php code with '<?php $x = 1;'
-  ,parseCode: function(buffer) {
-    this.lexer.mode_eval = false;
-    this.lexer.all_tokens = false;
-    return this.parser.parse(buffer);
-  }
-  ,parser: null
-  // lexer instance
-  ,lexer: require('./src/lexer')
-  // tokens dictionnary
-  ,tokens: require('./src/tokens')
+	// parsing eval string as '$x = 1;'
+	parseEval: function(buffer, options) {
+		var lexer = this.lexer;
+		var parser = this.parser;
+		if (options) {
+			if (options.hasOwnProperty('lexer')) {
+				lexer = extend(lexer, options.lexer);
+			}
+			if (options.hasOwnProperty('parser')) {
+				parser = extend(parser, options.parser);
+			}
+			parser.lexer = lexer;
+		}
+		lexer.mode_eval = true;
+		lexer.all_tokens = false;
+		return parser.parse(buffer);
+	}
+	// parse php code with '<?php $x = 1;'
+	,parseCode: function(buffer, options) {
+		var lexer = this.lexer;
+		var parser = this.parser;
+		if (options) {
+			if (options.hasOwnProperty('lexer')) {
+				lexer = extend(lexer, options.lexer);
+			}
+			if (options.hasOwnProperty('parser')) {
+				parser = extend(parser, options.parser);
+			}
+			parser.lexer = lexer;
+		}
+		lexer.mode_eval = false;
+		lexer.all_tokens = false;
+		return parser.parse(buffer);
+		}
+	}
+	,parser: null
+	// lexer instance
+	,lexer: require('./src/lexer')
+	// tokens dictionnary
+	,tokens: require('./src/tokens')
 };
 // parser instance
 engine.parser = require('./src/parser')(engine);
