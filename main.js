c61b81a8907d45b1083cec0bf0b67cfa1f7e28f8
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-04-15 13:32:58 +0200

    add a new create helper function to instanciate a new parser context

--- main.js
+++ main.js
@@ -4,7 +4,44 @@
  * @url http://glayzzle.com
  */
 var extend = require('util')._extend;
+
+// helper to clone an object
+var deepCopy = function(origin, add) {
+  if (!add || typeof add !== 'object') return origin;
+  var keys = Object.keys(add);
+  var i = keys.length;
+  while (i--) {
+    var assign = add[keys[i]];
+    if (typeof assign === 'object' && !Array.isArray(assign)) {
+      if (!origin.hasOwnProperty(keys[i])) {
+        origin[keys[i]] = {};
+      }
+      origin[keys[i]] = deepCopy(origin[keys[i]], assign);
+    } else {
+      origin[keys[i]] = assign;
+    }
+  }
+  return origin;
+};
+
+
 var engine = {
+
+  // creates a new instance (for multiple parsing cases)
+  create: function(options) {
+    var result = deepCopy({}, this);
+    if (options) {
+      if (options.hasOwnProperty('lexer')) {
+        result.lexer = deepCopy(result.lexer, options.lexer);
+      }
+      if (options.hasOwnProperty('parser')) {
+        result.parser = deepCopy(result.parser, options.parser);
+      }
+      result.parser.lexer = result.lexer;
+    }
+    return result;
+  },
+
   // parsing eval string as '$x = 1;'
   parseEval: function(buffer, options) {
     var lexer = this.lexer;
