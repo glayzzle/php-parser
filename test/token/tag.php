ae0dfc848cc287344af061c4afb6bf196e540f3b
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-10-05 12:53:59 +0200

    improve coverage on tags

---
+++ test/token/tag.php
@@ -0,0 +1,8 @@
+<%= "this is asp mode tag"; // %>
+<% echo "this is asp mode tag"; // %>
+<?xml version="1.0">
+<? echo "short tag" ?>
+<?phparhg!?>
+<?php /*/ ?>
+<?php /**/ ?>
+<?php echo "$var?"; ?>
