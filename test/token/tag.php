89bb053b4df205015827975c7108b0ac922cda10
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-12-28 13:06:02 +0100

    https://github.com/glayzzle/php-parser-tests/issues/1

--- test/token/tag.php
+++
@@ -1,8 +0,0 @@
-<%= "this is asp mode tag"; // %>
-<% echo "this is asp mode tag"; // %>
-<?xml version="1.0">
-<? echo "short tag" ?>
-<?phparhg!?>
-<?php /*/ ?>
-<?php /**/ ?>
-<?php echo "$var?"; ?>
