47e2af6745b746820989f0eaa4beafb8d10f0a2a
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-03-21 15:50:36 +0100

    release 2.0.3

--- dist/php-parser.js
+++ dist/php-parser.js
@@ -1,4 +1,4 @@
-/*! php-parser - BSD3 License - 2017-03-08 */
+/*! php-parser - BSD3 License - 2017-03-21 */
 
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // shim for using process in browser
@@ -6259,11 +6259,13 @@ module.exports = {
     if (this.token !== ')') {
       while(this.token != this.EOF) {
         var argument = this.read_argument_list();
-        result.push(argument);
-        if (argument.kind === 'variadic') {
-          wasVariadic = true;
-        } else if (wasVariadic) {
-          this.raiseError('Unexpected argument after a variadic argument');
+        if (argument) {
+          result.push(argument);
+          if (argument.kind === 'variadic') {
+            wasVariadic = true;
+          } else if (wasVariadic) {
+            this.raiseError('Unexpected argument after a variadic argument');
+          }
         }
         if (this.token === ',') {
           this.next();
