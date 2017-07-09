1d2ce7c87d2bd2dd89ea00562eb4f664ae0ae87c
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-07-09 22:15:05 +0200

    https://github.com/glayzzle/php-parser/issues/73 add a curly boolean on variable node

--- src/ast/variable.js
+++ src/ast/variable.js
@@ -12,13 +12,25 @@ var KIND = 'variable';
  * be any expression in general, an expression can also be a pattern.
  * @constructor Variable
  * @extends {Expression}
- * @property {String|Node} name
- * @property {boolean} byref
+ * @example
+ * // PHP code :
+ * &$foo
+ * // AST output
+ * {
+ *  "kind": "variable",
+ *  "name": "foo",
+ *  "byref": true,
+ *  "curly": false
+ * }
+ * @property {String|Node} name The variable name (can be a complex expression when the name is resolved dynamically)
+ * @property {boolean} byref Indicate if the variable reference is used, ex `&$foo`
+ * @property {boolean} curly Indicate if the name is defined between curlies, ex `${foo}`
  */
-var Variable = Expr.extends(function Variable(name, byref, location) {
+var Variable = Expr.extends(function Variable(name, byref, curly, location) {
   Expr.apply(this, [KIND, location]);
   this.name = name;
   this.byref = byref || false;
+  this.curly = curly || false;
 });
 
 module.exports = Variable;
