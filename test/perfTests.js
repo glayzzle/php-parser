f72e82a2383fd7c524ded33236c73e03911936b1
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-12-28 13:53:11 +0100

    reduce iteration to pass tests on low backends

--- test/perfTests.js
+++ test/perfTests.js
@@ -19,7 +19,7 @@ describe("Performance tests", function() {
     var reader = new parser();
     var tokSize = reader.tokenGetAll(code).length;
     var hrTime = process.hrtime();
-    var iter = 500;
+    var iter = 100;
     var start = hrTime[0] * 1000000 + hrTime[1] / 1000;
     for (var i = 0; i < iter; i++) {
       reader.tokenGetAll(code);
@@ -78,7 +78,7 @@ describe("Performance tests", function() {
     countNode(reader.parseCode(code));
 
     var hrTime = process.hrtime();
-    var iter = 500;
+    var iter = 100;
     var start = hrTime[0] * 1000000 + hrTime[1] / 1000;
     for (var i = 0; i < iter; i++) {
       reader.parseCode(code);
