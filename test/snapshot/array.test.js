f5d121663bef46de92912a37895a6d4216e5b3c9
Christian Zosel <christian@zosel.ch>
2018-04-22 11:14:03 +0200

    Add array tests

---
+++ test/snapshot/array.test.js
@@ -0,0 +1,60 @@
+const parser = require('../../src/index');
+
+describe('Array without keys', () => {
+  it('deference array', () => {
+    expect(
+      parser.parseEval(
+        ['$a = [', '"a", "b"', ']($foo)[$foo->bar()[1]]->foo()'].join('\r'),
+      ),
+    ).toMatchSnapshot();
+  });
+
+  it('of strings', () => {
+    expect(
+      parser.parseEval('array("item1", "item2", "item3")'),
+    ).toMatchSnapshot();
+  });
+
+  it('of numbers', () => {
+    expect(parser.parseEval('array(1, 2.5, 0x1000)')).toMatchSnapshot();
+  });
+
+  it('of strings and numbers', () => {
+    expect(parser.parseEval('array(1, "item2", 3, "item4")')).toMatchSnapshot();
+  });
+
+  it('of variables', () => {
+    expect(parser.parseEval('array($obj1, $obj2, $obj3)')).toMatchSnapshot();
+  });
+
+  it('of objects', () => {
+    expect(
+      parser.parseEval('[new foo(), new stdClass(), new bar()]'),
+    ).toMatchSnapshot();
+  });
+
+  it('of arrays', () => {
+    expect(
+      parser.parseEval(
+        [
+          'array(',
+          '  array("item1", "item2"),',
+          '  array("item3", "item4"),',
+          '  array("item5", "item6")',
+          ')',
+        ].join('\n'),
+      ),
+    ).toMatchSnapshot();
+  });
+
+  describe('mixed tests / coverage', function() {
+    it('test empty array', function() {
+      expect(parser.parseEval('$a = []; $b = array();')).toMatchSnapshot();
+    });
+    it('test short form / keys', function() {
+      expect(
+        parser.parseEval('[0 => &$foo, $bar => "foobar"];'),
+      ).toMatchSnapshot();
+    });
+  });
+});
