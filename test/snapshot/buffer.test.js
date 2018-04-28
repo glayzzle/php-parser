9728793b7f84bf69069dd10dd173fb15f5065ace
Ioan CHIRIAC <i.chiriac@yahoo.com>
2018-04-28 23:16:32 +0200

    migrate buffer & class

--- test/snapshot/buffer.test.js
+++ test/snapshot/buffer.test.js
@@ -1,9 +1,8 @@
-var parser = require("./main");
+const parser = require("../../src/index");
 
 describe("Test buffers", function() {
   it("handles buffers as input", function() {
     var buffer = new Buffer('echo "test"');
-    var ast = parser.parseEval(buffer);
-    ast.children[0].kind.should.be.exactly("echo");
+    expect(parser.parseEval(buffer)).toMatchSnapshot();
   });
 });
