9ca22810ecb1f7ab568f52ea3efa0000276c4c4c
Filippo Conti <filippo@codekraft.it>
2018-07-08 17:34:37 +0200

    Added webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const {name, description, license, author} = require('./package.json');
+
+const entry = './src/index.js';
+
+const entries = {
+  [`${name}`]: entry,
+  [`${name}.min`]: entry
+};
+
+const today = `${new Date(Date.now()).toLocaleDateString()}`;
+
+module.exports = {
+  target: 'web',
+  mode: 'production',
+  entry: entries,
+  output: {
+    filename: `[name].js`,
+    path: path.resolve(__dirname, 'dist'),
+    library: 'PhpParser',
+    libraryExport: 'default',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader?cacheDirectory=true',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new UglifyJsPlugin({
+      include: /\.min\.js$/,
+      sourceMap: true,
+      uglifyOptions: {
+        compress: {
+          keep_fnames: true
+        },
+        sourceMap: true,
+        mangle: false,
+        maxLineLen: 1024
+      }
+    })]
+  },
+  plugins: [
+		new webpack.BannerPlugin({
+      entryOnly: true,
+      banner: `
+        Package: ${name}
+        ${description}
+        Build: [hash]
+        License: ${license}
+        Author: ${author}
+      `
+		})
+	]
+};
