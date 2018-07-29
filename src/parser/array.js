41dee0216cc73f5edfc676bc1492e138124e580e
Ioan CHIRIAC <i.chiriac@yahoo.com>
2018-07-29 12:46:16 +0200

    https://github.com/prettier/plugin-php/issues/246 : let trailling commas pass

--- src/parser/array.js
+++ src/parser/array.js
@@ -33,13 +33,13 @@ module.exports = {
       items = this.read_array_pair_list(shortForm);
     }
     // check non empty entries
-    items.forEach(function(item) {
-      if (item === null) {
+    for(let i = 0, size = items.length - 1; i < size; i++) {
+      if (items[i] === null) {
         this.raiseError(
           "Cannot use empty array elements in arrays"
         );
       }
-    }.bind(this));
+    }
     this.expect(expect);
     this.next();
     return result(shortForm, items);
