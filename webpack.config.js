6bd1327fe258fe250865c00cb562871641955024
Filippo Conti <filippo@codekraft.it>
2018-07-29 10:29:24 +0200

    Codeclimate fix
    Ignore tutorials and docs

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader?cacheDirectory=true'
+          loader: "babel-loader?cacheDirectory=true"
         }
       }
     ]
