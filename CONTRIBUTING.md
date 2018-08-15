e74272803f06f75fbcc7a36cdcf26cf4ec4b3872
Filippo Conti <filippo@codekraft.it>
2018-08-15 21:50:16 +0200

    Added docs and publish command
    Added documentation

--- CONTRIBUTING.md
+++
@@ -1,3 +0,0 @@
-Everyone is welcome to contribute, just use the same coding style, and cover with tests your code. 
-
-Any PR is welcomed :)
--- test/snapshot/buffer.test.js
+++
@@ -1,8 +0,0 @@
-const parser = require('../main');
-
-describe("Test buffers", function() {
-  it("handles buffers as input", function() {
-    var buffer = new Buffer('echo "test"');
-    expect(parser.parseEval(buffer)).toMatchSnapshot();
-  });
-});
