f5f023b836bc4ec8f46cea616307d5f9fa823f22
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-09-23 21:51:11 +0300

    test: simple_variable (#395)

--- src/parser/variable.js
+++ src/parser/variable.js
@@ -333,7 +333,6 @@ module.exports = {
           break;
         }
         case "$": // $$$var
-          // @fixme check coverage here
           result = result(this.read_simple_variable(), false);
           break;
         case this.tok.T_VARIABLE: {
