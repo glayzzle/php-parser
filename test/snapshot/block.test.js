e9151259288280834f2b07922201b66a0548a2e2
Ioan CHIRIAC <i.chiriac@yahoo.com>
2020-01-13 23:18:47 +0100

    update tests on empty blocks

--- test/snapshot/block.test.js
+++ test/snapshot/block.test.js
@@ -1,7 +1,20 @@
 const parser = require("../main");
 
 describe("block", () => {
-  it("single", () => {
-    expect(parser.parseEval("{ $var = 1; }")).toMatchSnapshot();
+  it.each([
+    ["single", "{ $var = 1; }"],
+    ["empty function block", "function foo() { /* 1 */ }"],
+    ["empty class block", "class foo { /* 1 */ }"],
+    ["empty method block", "class foo { function bar()  { /* 1 */ } }"],
+    ["empty namespace block", "namespace foo { /* 1 */ }"],
+    ["empty declare block", "declare(tick=1) { /* 1 */ }"]
+  ])("%s", function(_, code) {
+    expect(
+      parser.parseEval(code, {
+        parser: {
+          extractDoc: true
+        }
+      })
+    ).toMatchSnapshot();
   });
 });
