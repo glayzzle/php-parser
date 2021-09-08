09f9e6983b75fcb66ca68f10030fde0c0d7f0d43
Maarten Staa <maarten.staa@brightalley.nl>
2021-09-08 14:56:49 +0200

    Add tests for enum parsing.

---
+++ test/snapshot/enum.test.js
@@ -0,0 +1,93 @@
+const parser = require("../main");
+
+describe("Test enums", function () {
+  it("empty", function () {
+    expect(parser.parseEval("enum Foo {}")).toMatchSnapshot();
+  });
+
+  it("non-empty", function () {
+    expect(
+      parser.parseEval(`
+        enum Suit {
+          case Hearts;
+          case Diamonds;
+          case Clubs;
+          case Spades;
+        }
+      `)
+    ).toMatchSnapshot();
+  });
+
+  it("may have a backing type", function () {
+    expect(
+      parser.parseEval(`
+        enum Suit: string {
+          case Hearts = 'H';
+          case Diamonds = 'D';
+          case Clubs = 'C';
+          case Spades = 'S';
+        }
+      `)
+    ).toMatchSnapshot();
+  });
+
+  it("can alias cases", function () {
+    expect(
+      parser.parseEval(`
+        enum Foo {
+          case Bar;
+          public const Baz = self::Bar;
+        }
+      `)
+    ).toMatchSnapshot();
+  });
+
+  it("can implement interfaces", function () {
+    expect(
+      parser.parseEval(`
+        enum Foo implements Bar, Baz {}
+      `)
+    ).toMatchSnapshot();
+  });
+
+  it("can use traits", function () {
+    expect(
+      parser.parseEval(`
+        enum Foo {
+          use Bar, Baz {
+            Baz::hello insteadof Bar;
+            Baz::world as earth;
+          }
+          use Bax;
+        }
+      `)
+    ).toMatchSnapshot();
+  });
+
+  it("can have functions", function () {
+    expect(
+      parser.parseEval(`
+        enum Foo: int {
+          case MyCase = 1;
+
+          public function bar(): void {}
+
+          protected static function baz() {
+            echo self::MyCase->value;
+          }
+        }
+      `)
+    ).toMatchSnapshot();
+  });
+
+  it("cannot have properties", function () {
+    expect(() => {
+      parser.parseEval(`
+        enum Foo {
+          public int $bar;
+          protected $baz;
+        }
+      `);
+    }).toThrowErrorMatchingSnapshot();
+  });
+});
