21170c9b65dce4d7dc55b778c843aecd36f39f86
Maarten Staa <maarten.staa@brightalley.nl>
2022-05-04 09:59:17 +0200

    Handle more bitwise operators.

--- src/lexer/attribute.js
+++ src/lexer/attribute.js
@@ -36,6 +36,9 @@ module.exports = {
       case "+":
       case "*":
       case "%":
+      case "~":
+      case "<":
+      case ">":
         return this.consume_TOKEN();
       case "[":
         listDepth++;
