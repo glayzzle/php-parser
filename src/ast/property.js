b24774be2817d77dcf433cd33a9bad43c477d96e
Jorg Sowa <jorg.sowa@gmail.com>
2025-12-20 00:55:26 +0100

    typo

--- src/ast/property.js
+++ src/ast/property.js
@@ -18,7 +18,7 @@ const KIND = "property";
  * @property {boolean} readonly
  * @property {boolean} nullable
  * @property {Identifier|Array<Identifier>|null} type
- * @propert {PropertyHook[]} hooks
+ * @property {PropertyHook[]} hooks
  * @property {AttrGroup[]} attrGroups
  */
 module.exports = Statement.extends(
